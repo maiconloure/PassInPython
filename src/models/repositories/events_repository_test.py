cf86c25cc9515722bb768c197f4319596438e313
Maicon Lourenço <maiconloure@gmail.com>
2024-04-09 23:46:15 -0300

    feat: Implementing events_repository

---
+++ src/models/repositories/events_repository_test.py
@@ -0,0 +1,25 @@
+from src.models.settings.connection import db_connection_handler
+from .events_repository import EventsRepository
+import pytest
+
+db_connection_handler.connect_to_db()
+
+@pytest.mark.skip(reason="New register in the database")
+def test_insert_event():
+    event = {
+        "uuid": "my-uuid-test",
+        "title": "My Title",
+        "slug": "my-slug",
+        "maximum_attendees": 10
+    }
+
+    events_repository = EventsRepository()
+    response = events_repository.insert_event(event)
+    print(response)
+
+
+def test_get_event_by_id():
+    event_id = "my-uuid-test"
+    events_repository = EventsRepository()
+    response = events_repository.get_event_by_id(event_id)
+    print(response)
