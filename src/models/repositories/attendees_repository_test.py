8bb1e34c1d887995602725b5a99c09816fda20f6
Maicon Lourenço <maiconloure@gmail.com>
2024-04-15 20:59:29 -0300

    feat: implementing AttendeesRepository

---
+++ src/models/repositories/attendees_repository_test.py
@@ -0,0 +1,25 @@
+from .attendees_repository import AttendeeRepository
+from src.models.settings.connection import db_connection_handler
+import pytest
+
+db_connection_handler.connect_to_db()
+
+@pytest.mark.skip(reason="New register in the database")
+def test_insert_attendee(): 
+    event_id = "my-uuid-test"
+    attendees_info = {
+        "uuid": "meu_uuid_attendee",
+        "name": "Attendee Name",
+        "email": "attendee@email.com",
+        "event_id": event_id
+    }
+    attendee_repository = AttendeeRepository()
+    response = attendee_repository.insert_attendee(attendees_info)
+    print(response)
+
+@pytest.mark.skip(reason="...")
+def test_get_attendee_badge_by_id():
+    attendee_id = "meu_uuid_attendee"
+    attendee_repository = AttendeeRepository()
+    response = attendee_repository.get_attendee_badge_by_id(attendee_id)
+    print(response)
