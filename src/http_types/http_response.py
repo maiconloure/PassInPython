7c6baaa96a6019802af77b7607b817c6e2e30d4d
Maicon Lourenço <maiconloure@gmail.com>
2024-04-17 01:06:21 -0300

    feat: Implementing event_routes

--- src/http_types/http_response.py
+++ src/http_types/http_response.py
@@ -3,4 +3,4 @@ from typing import Dict
 class HttpResponse:
     def __init__(self, body: Dict, status_code: int) -> None:
         self.body = body
-        self.status_code = status_code 
+        self.status_code = status_code
